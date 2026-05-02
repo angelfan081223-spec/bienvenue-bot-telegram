d8224e62418876254ef8b15ff5c39d0618577e84
angelfan081223-spec <angelfan081223@gmail.com>
2026-05-02 17:01:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,20 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ChatMemberHandler, ContextTypes
 
-TOKEN = os.environ.get("TOKEN")
+TOKEN = "8709983426:AAFmP05ovFcDM__Pfpf-PTjaFaim9vGsptA"
 
 async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
     result = update.chat_member
     if result.new_chat_member.status == "member":
         user = result.new_chat_member.user
-        await context.bot.send_message(
+        await context.bot.send_photo(
             chat_id=user.id,
-            text="Coucou 😘\nBienvenue sur mon canal telegram !\n\nTon message ici..."
+            photo="https://LIEN_DIRECT_VERS_TA_PHOTO.jpg",
+            caption=(
+                "Coucou ♥️
+Trop cool que tu sois venue sur mon telegram privé !\n\nSi ça te dit je te donne un accès gratuit pour mon OF pour aujourd’hui pour qu’on puisse faire connaissance 😏"
+                "https://onlyfans.com/amanda.lpz/c66"
+            )
         )
 
 app = ApplicationBuilder().token(TOKEN).build()
